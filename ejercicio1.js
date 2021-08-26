eb56558d5a709ddc4ab310a7d8cb95f4affafb84
daar026 <darwinsantiago2003@hotmail.com>
2021-08-26 16:21:34 -0500

    intento

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,6 +1,6 @@
 function mmi(n1, n2) { 
     if (n1 > n2) {
-        return console.log("El número " + n1 + " es mayor que el número " + n2)
+        return console.log("El número" + n1 + " es mayor que el número " + n2)
     }
     else 
         if (n1 < n2) {
